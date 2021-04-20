cf84e252a0a9a5e41785407ceba97d28dc940868
Serhii <Kistanov.serhii@yandex.ru>
2021-04-20 18:20:41 +0300

    init

--- lib/presentation/home/components/table.dart
+++ lib/presentation/home/components/table.dart
@@ -11,88 +11,97 @@ Widget table() {
         padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
         child: GridView.builder(
           itemCount: voices.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          /*gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 1,
             mainAxisSpacing: kDefaultPaddin,
             crossAxisSpacing: kDefaultPaddin,
             //childAspectRatio: 0.75,
-          ),
-          itemBuilder: (context, index) =>
-              VoiceBox(
-                  voice: voices[index],
-                  press: () => {} /*
+          ),*/
+          itemBuilder: (context, index) => VoiceBox(
+              voice: voices[index],
+              press: () =>
+                  {} /*
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => DetailsScreen(
                         product: products[index],
                         ),
-                      )*/),
+                      )*/
+              ),
         )),
   );
 }
 
-
-
 List<Voice> getVoices() {
   return [
     new Voice(
-        cover: Image(image: AssetImage('assets/images/cover1.jpg'), width: 120, height: 150,),
+        cover: Image(
+          image: AssetImage('assets/images/cover1.jpg'),
+          width: 120,
+          height: 150,
+        ),
         equalizer: Image(image: AssetImage('assets/images/equalizer.png')),
         timeInfo: "5m",
         statusBtn: Icon(Icons.access_alarms),
         title: "Hello world!",
         comments: "A10k",
-        countListned: "L15K"
-    ),
-
+        countListned: "L15K"),
     new Voice(
-        cover: Image(image: AssetImage('assets/images/cover2.jpg'), width: 120, height: 150,),
+        cover: Image(
+          image: AssetImage('assets/images/cover2.jpg'),
+          width: 120,
+          height: 150,
+        ),
         equalizer: Image(image: AssetImage('assets/images/equalizer.png')),
         timeInfo: "7m",
         statusBtn: Icon(Icons.access_alarms),
         title: "Hello world!",
         comments: "A1k",
-        countListned: "L2K"
-    ),
-
+        countListned: "L2K"),
     new Voice(
-        cover: Image(image: AssetImage('assets/images/cover3.jpg'), width: 120, height: 150),
+        cover: Image(
+            image: AssetImage('assets/images/cover3.jpg'),
+            width: 120,
+            height: 150),
         equalizer: Image(image: AssetImage('assets/images/equalizer.png')),
         timeInfo: "2m",
         statusBtn: Icon(Icons.access_alarms),
         title: "Hello world!",
         comments: "A120",
-        countListned: "L320"
-    ),
-
+        countListned: "L320"),
     new Voice(
-        cover: Image(image: AssetImage('assets/images/cover1.jpg'), width: 120, height: 150),
+        cover: Image(
+            image: AssetImage('assets/images/cover1.jpg'),
+            width: 120,
+            height: 150),
         equalizer: Image(image: AssetImage('assets/images/equalizer.png')),
         timeInfo: "5m",
         statusBtn: Icon(Icons.access_alarms),
         title: "Hello world!",
         comments: "A75",
-        countListned: "L320"
-    ),
+        countListned: "L320"),
     new Voice(
-        cover: Image(image: AssetImage('assets/images/cover3.jpg'), width: 120, height: 150),
+        cover: Image(
+            image: AssetImage('assets/images/cover3.jpg'),
+            width: 120,
+            height: 150),
         equalizer: Image(image: AssetImage('assets/images/equalizer.png')),
         timeInfo: "5m",
         statusBtn: Icon(Icons.access_alarms),
         title: "Hello world!",
         comments: "A75",
-        countListned: "L320"
-    ),
+        countListned: "L320"),
     new Voice(
-        cover: Image(image: AssetImage('assets/images/cover2.jpg'), width: 120, height: 150),
+        cover: Image(
+            image: AssetImage('assets/images/cover2.jpg'),
+            width: 120,
+            height: 150),
         equalizer: Image(image: AssetImage('assets/images/equalizer.png')),
         timeInfo: "5m",
         statusBtn: Icon(Icons.access_alarms),
         title: "Hello world!",
         comments: "A75",
-        countListned: "L320"
-    ),
-
+        countListned: "L320"),
   ];
 }
